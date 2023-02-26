73b84b0763eb875aa77ba215cf297755008ff2fe
raghava139 <rraghava139@gmail.com>
2023-02-26 22:41:16 +0530

    master_1

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,38 +1,38 @@
-import { Router } from "express";
-import User from "../models/user";
-import bcrypt from 'bcrypt';
-import Joi from "joi";
-import passwordComplexity from "joi-password-complexity";
+// import { Router } from "express";
+// import User from "../models/user";
+// import bcrypt from 'bcrypt';
+// import Joi from "joi";
+// import passwordComplexity from "joi-password-complexity";
 
-// Here, router is an instance of the Express router
-const router=Router();
+// // Here, router is an instance of the Express router
+// const router=Router();
 
- // function to handle the HTTP POST request
-router.post('/users', async(req, res)=> {
-// req--->retrieve data from the request body or params
-//  res--->send a response to the client
+//  // function to handle the HTTP POST request
+// router.post('/users', async(req, res)=> {
+// // req--->retrieve data from the request body or params
+// //  res--->send a response to the client
 
-    try{
-      const {error}=signUpBodyValidation(req.body);
-      if(error){
-        return res.status(400).json({error:true,message:error.details})
-      }
+//     try{
+//       const {error}=signUpBodyValidation(req.body);
+//       if(error){
+//         return res.status(400).json({error:true,message:error.details})
+//       }
 
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({error:true,message:"Internal Server Error"})
-    }
-  });
+//     }
+//     catch(err){
+//         console.log(err);
+//         res.status(500).json({error:true,message:"Internal Server Error"})
+//     }
+//   });
 
-  const signUpBodyValidation=(body)=>{
-    //  syntax-->Joi.object([schema])-->it is might be use to valdate an incoming json payload example --> userName,email,password
-      const schema=Joi.object({
-          userName:Joi.string().required().label('user Name'),
-          email:Joi.string().email().required().label('Email'),
-        //here the password commplexity()--->It can help prevent the weak passwords from being improve the overal security of your application
-          password:passwordComplexity()
+//   const signUpBodyValidation=(body)=>{
+//     //  syntax-->Joi.object([schema])-->it is might be use to valdate an incoming json payload example --> userName,email,password
+//       const schema=Joi.object({
+//           userName:Joi.string().required().label('user Name'),
+//           email:Joi.string().email().required().label('Email'),
+//         //here the password commplexity()--->It can help prevent the weak passwords from being improve the overal security of your application
+//           password:passwordComplexity()
 
-      })
-      return schema.validate(body)
-  }
+//       })
+//       return schema.validate(body)
+//   }
